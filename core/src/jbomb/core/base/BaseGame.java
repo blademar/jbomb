07711b85b35fcd7d7f00b421adcc1fc2cd23fb3b
villa061004 <villa061004@gmail.com>
2011-12-02 04:44:35 +0000

    Juego Base sin main

--- core/src/jbomb/core/base/BaseGame.java
+++ core/src/jbomb/core/base/BaseGame.java
@@ -3,7 +3,7 @@ package jbomb.core.base;
 import com.jme3.app.SimpleApplication;
 import com.jme3.system.AppSettings;
 
-public class BaseGame extends SimpleApplication {
+public abstract class BaseGame extends SimpleApplication {
     
     private AppSettings appSettings = new AppSettings(true);
     
@@ -11,17 +11,9 @@ public class BaseGame extends SimpleApplication {
         initAppSettings();
     }
     
-    public static void main(String[] args) {
-        new BaseGame().start();
-    }
-
-    @Override
-    public void simpleInitApp() {}
-    
     private void initAppSettings() {
         appSettings.setResolution(640, 480);
         setSettings(appSettings);
         setShowSettings(false);
     }
-    
 }
