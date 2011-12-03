08400a46d15296639bcf40fd644faa3e4f51d652
villa061004@gmail.com <villa061004@gmail.com@72f25e8e-2a27-244b-eafe-869afaa549a8>
2011-12-03 05:30:06 +0000

    Eliminando archivos inecesarios

--- trunk/core/src/jbomb/core/game/JBomb.java
+++ trunk/core/src/jbomb/core/game/JBomb.java
@@ -12,27 +12,16 @@ import jbomb.core.utils.MatDefs;
 
 public class JBomb extends BaseGame {
 
-    private final String boxesPath = "textures/boxes/";
-
     @Override
     public void simpleInitApp() {
-        final float separation = -.15f;
-
-        flyCam.setMoveSpeed(40);
-
+        super.simpleInitApp();
         initSky();
         initFloor();
-        makeWall(0, 0, 20, "w_gray1.png", "w_blue.png", separation);
-        makeWall(5, 0, 20, "w_gray1.png", "w_darkblue.png", separation);
-        makeWall(0, 2, 20, "w_gray1.png", "w_red1.png", separation);
-        makeWall(5, 2, 20, "w_gray1.png", "w_red2.png", separation);
-        makeWall(-5, 0, 20, "w_gray1.png", "w_yellow.png", separation);
-        makeWall(-10, 0, 20, "w_gray1.png", "w_brown.png", separation);
-        makeWall(-5, 2, 20, "w_gray1.png", "w_green1.png", separation);
-        makeWall(-10, 2, 20, "w_gray1.png", "w_green3.png", separation);
+        initScene();
     }
 
     private void makeWall(float x, float y, float z, String primaryTexture, String secundaryTexture, float separation) {
+        String boxesPath = "textures/boxes/";
         makeCube(.5f, .5f, .5f, "StaticWall", boxesPath, primaryTexture, new Vector3f(x + .5f, y + 1.5f, z + .5f), new Vector2f(.5f, .5f));
         makeCube(.5f, .5f, .5f, "StaticWall", boxesPath, primaryTexture, new Vector3f(x + .5f, y + .5f, z + .5f), new Vector2f(.5f, .5f));
 
@@ -47,6 +36,18 @@ public class JBomb extends BaseGame {
 
         makeCube(.5f, .5f, .5f, "StaticWall", boxesPath, secundaryTexture, new Vector3f(x + 4.5f, y + .5f, z + .5f), new Vector2f(.5f, .5f));
         makeCube(.5f, .5f, .5f, "StaticWall", boxesPath, primaryTexture, new Vector3f(x + 4.5f, y + 1.5f, z + .5f + separation), new Vector2f(.5f, .5f));
+    }
+    
+    private void initScene() {
+        float separation = -.15f;
+        makeWall(0, 0, 20, "w_gray1.png", "w_blue.png", separation);
+        makeWall(5, 0, 20, "w_gray1.png", "w_darkblue.png", separation);
+        makeWall(0, 2, 20, "w_gray1.png", "w_red1.png", separation);
+        makeWall(5, 2, 20, "w_gray1.png", "w_red2.png", separation);
+        makeWall(-5, 0, 20, "w_gray1.png", "w_yellow.png", separation);
+        makeWall(-10, 0, 20, "w_gray1.png", "w_brown.png", separation);
+        makeWall(-5, 2, 20, "w_gray1.png", "w_green1.png", separation);
+        makeWall(-10, 2, 20, "w_gray1.png", "w_green3.png", separation);
     }
 
     private void makeCube(float x, float y, float z, String name, String texturePath, String textureName, Vector3f localTranslation, Vector2f scaleTexture) {
@@ -71,7 +72,7 @@ public class JBomb extends BaseGame {
     }
 
     private void initFloor() {
-        makeCube(20f, 0.1f, 20f, "floor", boxesPath, "f_blue.png", new Vector3f(0f, -0.1f, 0f), new Vector2f(20f, 20f));
+        makeCube(20f, 0.1f, 20f, "floor", "floor", "f_blue.png", new Vector3f(0f, -0.1f, 0f), new Vector2f(20f, 20f));
     }
 
     private void initSky() {
