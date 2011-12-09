09cb2def5718f0e11acf751694eeca8752555b3e
villa061004 <villa061004@gmail.com>
2011-12-09 20:32:06 +0000

    Nuevas Clases Base e implementaciones por defecto

---
+++ core/src/jbomb/core/controls/BaseBombControl.java
@@ -0,0 +1,32 @@
+package jbomb.core.controls;
+
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.control.Control;
+import jbomb.core.weapons.api.Bomb;
+
+public class BaseBombControl extends JBombAbstractControl {
+    
+    private float time = 0f;
+    private Bomb bomb;
+
+    @Override
+    protected Control newInstanceOfMe() {
+        return new BaseBombControl();
+    }
+
+    @Override
+    protected void controlUpdate(float tpf) {
+        if (time >= bomb.getTimeForExplosion())
+            bomb.exploit();
+        else
+            time += tpf;
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {}
+
+    public void setBomb(Bomb bomb) {
+        this.bomb = bomb;
+    }
+}
