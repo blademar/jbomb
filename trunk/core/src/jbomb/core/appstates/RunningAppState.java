c5ed385ad2203264221c77e17aff66c194402e78
villa061004@gmail.com <villa061004@gmail.com@72f25e8e-2a27-244b-eafe-869afaa549a8>
2011-12-13 01:33:06 +0000

    Listener escuchando sonidos

--- trunk/core/src/jbomb/core/appstates/RunningAppState.java
+++ trunk/core/src/jbomb/core/appstates/RunningAppState.java
@@ -1,15 +1,22 @@
 package jbomb.core.appstates;
 
 import com.jme3.app.state.AbstractAppState;
+import com.jme3.audio.Listener;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
 import jbomb.core.game.JBombContext;
 
 public class RunningAppState extends AbstractAppState {
     
     @Override
     public void update(float tpf) {
-        Vector3f camDir = JBombContext.JBOMB.getCam().getDirection().clone().multLocal(0.2f);
-        Vector3f camLeft = JBombContext.JBOMB.getCam().getLeft().clone().multLocal(0.1f);
+        moveCam();
+    }
+    
+    private void moveCam() {
+        Camera cam = JBombContext.JBOMB.getCam();
+        Vector3f camDir = cam.getDirection().clone().multLocal(0.2f);
+        Vector3f camLeft = cam.getLeft().clone().multLocal(0.1f);
         camDir.y = 0;
         camLeft.y = 0;
         Vector3f walk = new Vector3f();
@@ -26,6 +33,13 @@ public class RunningAppState extends AbstractAppState {
         
         JBombContext.JBOMB.getPlayer().setWalkDirection(walk);
         JBombContext.JBOMB.getCam().setLocation(JBombContext.JBOMB.getPlayer().getPhysicsLocation());
+    }
+    
+    private void hearingSounds() {
+        Camera cam = JBombContext.JBOMB.getCam();
+        Listener listener = JBombContext.JBOMB.getListener();
+        listener.setLocation(cam.getLocation());
+        listener.setRotation(cam.getRotation());
     }
     
 }
