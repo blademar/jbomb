ff00d0cd4140b0c1bc3e56d4bd6c273561c8eae5
villa061004@gmail.com <villa061004@gmail.com@72f25e8e-2a27-244b-eafe-869afaa549a8>
2012-01-12 14:51:16 +0000

    Pruebas sin interpolacion

--- trunk/JBombCommon/src/jbomb/common/messages/ElevatorMovesMessage.java
+++ trunk/JBombCommon/src/jbomb/common/messages/ElevatorMovesMessage.java
@@ -5,10 +5,12 @@ import com.jme3.math.Vector3f;
 import com.jme3.network.serializing.Serializable;
 import jbomb.common.game.JBombContext;
 import jbomb.common.scene.Elevator;
+import org.apache.log4j.Logger;
 
 @Serializable
 public class ElevatorMovesMessage extends BasePhysicMessage {
 
+    private static final Logger LOGGER = Logger.getLogger(ElevatorMovesMessage.class);
     private Vector3f location = new Vector3f();
     private Vector3f oldPosition = new Vector3f();
     private boolean isFirstInterpolation;
@@ -40,6 +42,7 @@ public class ElevatorMovesMessage extends BasePhysicMessage {
                 isFirstInterpolation = true;
             }
             e.getGeometry().setLocalTranslation(FastMath.interpolateLinear(percent, oldPosition, location));
+            LOGGER.debug("Interpolando elevador");
         }
     }
 }
