e21109496d06e7e7975804d18372093e32296735
villa061004 <villa061004@gmail.com>
2012-01-23 03:32:48 +0000

    Agregado recursos para marcar la salud y color amarillo para transparencias internas

--- JBombServer/src/jbomb/server/listeners/BombCollisionListener.java
+++ JBombServer/src/jbomb/server/listeners/BombCollisionListener.java
@@ -20,11 +20,11 @@ public class BombCollisionListener implements PhysicsCollisionListener {
             if (nodeA.getParent() == ServerContext.NODE_PLAYERS
                     && ("bomb".equals(nodeB.getName()) || "wormsExplosion".equals(nodeB.getName()))) {
                 nodeA.getControl(ScorePlayerControl.class).sumDamage(1);
-                LOGGER.debug("Collision: " + nodeA.getName() + " with " + nodeB.getName());
+//                LOGGER.debug("Collision: " + nodeA.getName() + " with " + nodeB.getName());
             } else if (nodeB.getParent() == ServerContext.NODE_PLAYERS
                     && ("bomb".equals(nodeA.getName()) || "wormsExplosion".equals(nodeA.getName()))) {
                 nodeB.getControl(ScorePlayerControl.class).sumDamage(1);
-                LOGGER.debug("Collision: " + nodeB.getName() + " with " + nodeA.getName());
+//                LOGGER.debug("Collision: " + nodeB.getName() + " with " + nodeA.getName());
             }
         }
     }
