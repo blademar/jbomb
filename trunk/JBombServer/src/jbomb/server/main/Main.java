feefbf507252d819da92c62a04a23784d30f0572
villa061004@gmail.com <villa061004@gmail.com@72f25e8e-2a27-244b-eafe-869afaa549a8>
2012-01-27 00:09:46 +0000

    Servidor gráfico opcional

--- trunk/JBombServer/src/jbomb/server/main/Main.java
+++ trunk/JBombServer/src/jbomb/server/main/Main.java
@@ -7,9 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
         String playersNumber = "four";
+        boolean graphic = false;
         if (args.length != 0) {
             playersNumber = args[0];
+            if ("true".equals(args[1]))
+                graphic = true;
+            else if ("false".equals(args[1]))
+                graphic = false;
         }
-        new JBombServer(playersNumber).start(Type.Headless);
+        if (graphic)
+            new JBombServer(playersNumber).start(Type.Display);
+        else
+            new JBombServer(playersNumber).start(Type.Headless);
     }
 }
