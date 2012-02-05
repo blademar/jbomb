aedb9d8c0d4602ea0a6701f98eead6360759df5e
villa061004 <villa061004@gmail.com>
2012-02-05 04:37:52 +0000

    Solucionado bug al modificar guiNode cuando se reinicia juego

---
+++ client/src/jbomb/client/messages/task/StartGameTask.java
@@ -0,0 +1,19 @@
+package jbomb.client.messages.task;
+
+import jbomb.client.game.ClientContext;
+import jbomb.common.messages.StartGameMessage;
+import org.apache.log4j.Logger;
+
+public class StartGameTask implements Task<StartGameMessage> {
+    
+    private static final Logger LOGGER = Logger.getLogger(StartGameTask.class);
+
+    @Override
+    public void doThis(StartGameMessage message) {
+        ClientContext.APP.addListeners();
+        ClientContext.APP.initInterfaces();
+        ClientContext.APP.initHealthMarker();
+        ClientContext.APP.startGame();
+        LOGGER.debug("Starting game...");
+    }
+}
